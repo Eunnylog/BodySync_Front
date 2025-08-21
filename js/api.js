849589ce12b7f8ff772903af738bd92e01d76442
Eunnylog <lllala0710@gmail.com>
2025-08-22 02:35:40 +0900

    ✨ 메인페이지 대시보드 구현

--- js/api.js
+++ js/api.js
@@ -16,6 +16,22 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('login-email').value = "";
         document.getElementById('login-password').value = "";
     });
+
+    injectNavbar();
+
+    const payload = localStorage.getItem('payload')
+    const dashboardCards = document.getElementById('dashboard-cards').querySelector('.row')
+    const loginRequiredMessage = document.getElementById('login-required-message')
+
+    if (payload) {
+        console.log(payload)
+        dashboardCards.classList.remove('d-none')
+        loginRequiredMessage.classList.add('d-none')
+        loadDashboardData()
+    } else {
+        dashboardCards.classList.add('d-none')
+        loginRequiredMessage.classList.remove('d-none')
+    }
 })
 
 
@@ -87,4 +103,75 @@ function googleLogin() {
     // 구글 로그인 구현 시
     const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&access_type=offline&include_granted_scopes=true&response_type=code&state=security_token%3D1%26url%3Dhttps://oauth2.example.com/callback&redirect_uri=${GOOGLE_REDIRECT_URI}&client_id=${GOOGLE_CLIENT_ID}`;
     window.location.href = GOOGLE_AUTH_URL;
+}
+
+
+// 대시보드 데이터
+async function loadDashboardData() {
+    console.log("대시보드 데이터")
+
+    try {
+        const response = await fetch(`${backend_base_url}/users/dashboard/`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            })
+
+        if (!response.ok) {
+            const errorData = await response.json()
+            throw new Error(`Failed to load dashboard data: ${errorData.detail || JSON.stringify(errorData)}`)
+        }
+
+        const dashboardData = await response.json()
+        console.log('대시보드 수신 성공', dashboardData)
+
+        // UI 업데이트
+        updateDashboardCards(dashboardData)
+    } catch (error) {
+        console.error('대시보드 데이터 로딩 중 오류 발생', error)
+    }
+}
+
+
+function updateDashboardCards(data) {
+    // 1. 식단 요약 카드 업데이트
+    const mealData = data.meal_summary
+    if (mealData) {
+        document.getElementById('meal-calories-display').innerText = `${parseFloat(mealData.today_calories).toFixed(0)}`
+        document.getElementById('meal-carbs-display').innerHTML = `${parseFloat(mealData.today_carbs).toFixed(1)}`
+        document.getElementById('meal-protein-display').innerText = `${parseFloat(mealData.today_protein).toFixed(1)}`
+        document.getElementById('meal-fat-display').innerText = `${parseFloat(mealData.today_fat).toFixed(1)}`
+    }
+
+    // 2. 운동 요약 카드 업데이트
+    const activityData = data.activity_summary
+    if (activityData) {
+        document.getElementById('activity-duration-display').innerText = `${activityData.today_duration_minutes}`
+        document.getElementById('activity-calories-display').innerText = `${parseFloat(activityData.today_calories_burned).toFixed(0)}` // 소수점 버리고 정수 표시
+    }
+
+    // 3. 단식 요약 카드 업데이트
+    const fastingData = data.fasting_summary
+    if (fastingData.status == "진행 중") {
+        document.getElementById('fast-start-btn').style.display = 'none'
+        document.getElementById('fast-end-btn').style.display = 'block'
+    } else {
+        document.getElementById('fast-start-btn').style.display = 'block'
+        document.getElementById('fast-end-btn').style.display = 'none'
+    }
+    if (fastingData) {
+        document.getElementById('fasting-status-display').innerText = `${fastingData.status}`
+        document.getElementById('fasting-duration-display').innerText = `${fastingData.duration_time}`
+        document.getElementById('fasting-remaining-display').innerText = `${fastingData.remaining_time}`
+    }
+
+    const inbodyData = data.inbody_summary
+    if (inbodyData) {
+        document.getElementById('inbody-weight-display').innerText = `${parseFloat(inbodyData.weight).toFixed(1)}`
+        document.getElementById('inbody-muscle-display').innerText = `${parseFloat(inbodyData.skeletal_muscle_mass_kg).toFixed(1)}`
+        document.getElementById('inbody-fat-display').innerText = `${parseFloat(inbodyData.body_fat_percentage).toFixed(1)}`
+    }
 }
